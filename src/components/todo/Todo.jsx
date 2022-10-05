c02fd8eaceeb24b010b363845d460633535cbef5
dyhwang <80233565+dyhwnag@users.noreply.github.com>
2022-10-06 01:43:44 +0900

    Todo컴포넌트 state 삭제
    부모 Layout컴포넌트의 setTodos가 호출되는 순간 Layout 컴포넌트가 랜더링을 새로하고 자식 컴포넌트들이 모조리 새로 생성된다는걸 알게됨
    그래서, Todo컴포넌트에 불필요했던 state를 제거했음

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import './style.css';
 
 const Todo = ({ todo, onChange, onRemove }) => {
-    const [isDone, setIsDone] = useState(todo.isDone)
-
     return (
         <div className="todo">
             <div className='w1'>
@@ -17,14 +15,11 @@ const Todo = ({ todo, onChange, onRemove }) => {
                     }}>삭제</button>
 
                     <button className='btns2' onClick={() => {
-                        setIsDone(!isDone)
-                        // Todo 컴포넌트는 isDone 상태를 가지고, isDone 상태가 변경되면 자동으로 다시 렌더링
-                        // !isDone으로 뒤집어서 넣기(state에 isDone)
-
-                        todo.isDone = !isDone // todo객체의 isDone을 반대로
-                        onChange(todo)//Layout 컴포넌트에 있는 onChangeHandler 호출
+                        todo.isDone = !todo.isDone
+                        onChange(todo)
                     }}
-                    >{isDone === true ? '취소' : '완료'}</button>
+                    >{todo.isDone === true ? '취소' : '완료'}</button>
+
                 </div>
             </div>
         </div>
